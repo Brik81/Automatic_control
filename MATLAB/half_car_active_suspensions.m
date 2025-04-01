371b8f4bed5cc34293695ca27099585b45cd6de3
Brik81 <alex280199@gmail.com>
2025-04-01 12:55:55 +0200

    UPDATE
    Modello non-linearizzato funzionante

--- MATLAB/half_car_active_suspensions.m
+++ MATLAB/half_car_active_suspensions.m
@@ -5,9 +5,9 @@ clear all
 %% initial conditions
 %p0 = 0; % [m] initial position
 %v0 = 0; % [m/s] initial speed
-x0 = [  100; %posizione centro di massa
-        1; %velocità centro di massa
-        1; % pitch angle
-        1; %velocità angolare pitch 
-        1; %road slope
-        1]; %altezza strada
+x0 = [  2; %posizione centro di massa
+        0; %velocità centro di massa
+        0;      % pitch angle
+        0;    %velocità angolare pitch 
+        0 ; %road slope
+        0]; %altezza strada
