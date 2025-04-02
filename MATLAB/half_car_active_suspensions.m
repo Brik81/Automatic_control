72d90068d081ea0d95675287c8f4bfebececaa5f
Brik81 <alex280199@gmail.com>
2025-04-02 12:27:05 +0200

    simulink update 2/04
    aggiunti blocchi h ed e

--- MATLAB/half_car_active_suspensions.m
+++ MATLAB/half_car_active_suspensions.m
@@ -5,9 +5,21 @@ clear all
 %% initial conditions
 %p0 = 0; % [m] initial position
 %v0 = 0; % [m/s] initial speed
-x0 = [  2; %posizione centro di massa
+x0 = [  0.5; %posizione centro di massa
         0; %velocità centro di massa
         0;      % pitch angle
         0;    %velocità angolare pitch 
         0 ; %road slope
         0]; %altezza strada
+
+%% equilibrium conditions prese dal sommo Nicola Mimmoso
+
+r0 = 0; % [m] reference deflection
+%p0 = r0+ell0; % [m] equilibrium length
+%v0 = 0; % [m/s] equilibrium speed
+%x0 = [p0; v0]; % equilibrium state
+d0 = 0; % [N] equilibrium disturbance
+%u0 = k*(p0-ell0)+g*m-d0;
+%y0 = p0-ell0;
+
+w0 = [d0; 0; r0];
